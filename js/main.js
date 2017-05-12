ea6f20296b5fbc0e7a13c60a48d4e1b7c981a47a
simona <simonaulloa@gmail.com>
2017-05-12 12:27:27 -0300

    tea-lady

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+function validacion(nombre, apellido, email, direccion, telefono){
 
-
-
-
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var email = document.getElementById("email").value;
+	var direccion = document.getElementById("direccion").value;
+	var telefono = document.getElementById("telefono").value;
+ 
+	if( !isNaN(valor) ) {
+	  return false;
+	}
+}
